9d2d880e433d8800bb3570b65271dac3c7cb7f7e
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-15 10:01:09 +0530

    Exploration Of Task Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {todo:newTodo, status:false}]);
+    setNewTodo('');
+  };
+  const handleTodoInput =(e)=>{
+    setNewTodo(e.target.value);
+  };
   return (
-    <h1>Dynamic Todo List</h1>
+    <main>
+      <h1>Dynamic Todo List</h1>
+      <form onSubmit={addTodo}>
+        <label htmlFor="newTodo">Enter New Todo</label>
+        <input type="text" id="newTodo" value={newTodo} onChange={handleTodoInput} />
+        <button>Submit</button>
+      </form>
+      <div>
+        <h3>All Tasks</h3>
+        <ul>
+          {todos.map((todo, i) => <li key={i}>task:{todo.todo} Status:{todo.status?'Complete':'Incomplete'}</li>)}
+        </ul>
+      </div>
+    </main>
   )
 }
 
