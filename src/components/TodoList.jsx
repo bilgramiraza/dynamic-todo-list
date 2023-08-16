51153d5e90b1fda2e003f5c8974e09bcd68ca30b
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-16 16:09:26 +0530

    Added Filter for Displaying Only Active or Completed Tasks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import TodoItem from './TodoItem';
 import { TodosContext } from './TodosContext';
 
 const TodoList = () => {
   const { todos } = useContext(TodosContext);
 
+  const [filter, setFilter]= useState('All');//States: All|Active|Completed
+
   return (
     <div>
+      <div>
+        <button onClick={()=>setFilter('All')}>All</button>
+        <button onClick={()=>setFilter('Active')}>Active</button>
+        <button onClick={()=>setFilter('Completed')}>Completed</button>
+      </div>
       <ul>
-        {todos.map(todo =>
-          <TodoItem key={todo.id} todo={todo} />
-        )}
+        {todos.map(todo =>{
+          if((filter==='Active' && !todo.status)||(filter==='Completed' && todo.status)) return ;
+
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
       </ul>
     </div>);
 };
