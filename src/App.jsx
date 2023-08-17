306645dc68010b52e01ad051e74fcafcea397ea6
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-17 15:13:52 +0530

    Setup Basic Layout for the site

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import { TodosProvider } from "./components/TodosContext";
 function App() {
   return (
     <TodosProvider>
-      <header>
-        <h1 className="">Dynamic Todo List</h1>
-      </header>
-      <TodoForm />
-      <TodoList />
+      <div className="flex flex-col w-screen lg:flex-row">
+        <div className="w-full lg:w-1/5 ">
+          <header className="text-center">
+            <h1 className="text-3xl font-bold font-serif">Dynamic Todo List</h1>
+          </header>
+          <TodoForm />
+        </div>
+        <div className="w-full lg:w-4/5">
+          <TodoList />
+        </div>
+      </div>
     </TodosProvider>
   );
 }
