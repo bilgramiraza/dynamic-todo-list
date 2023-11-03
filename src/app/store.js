ef1ceb48fd6e051aca0cbd760290b6e263c010d1
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-03 11:01:54 +0530

    Removed Obsolete TodoSlice from the Application Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import todosReducer from "../features/todos/todosSlice";
 import filtersReducer from "../features/filters/filtersSlice";
 import { apiSlice } from "../features/api/apiSlice";
 
 export default configureStore({
   reducer:{
-    // todos: todosReducer,
     filters: filtersReducer,
     [apiSlice.reducerPath]:apiSlice.reducer,
   },
