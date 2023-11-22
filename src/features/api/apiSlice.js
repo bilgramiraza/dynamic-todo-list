af8993dd52f4f4a877b3f06bb711dee103ff59af
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-22 11:25:54 +0530

    Moved Backend API URI from the app to external Env file

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_API_URI }),
   tagTypes: ['Todo'],
   endpoints: builder => ({
     fetchTodos: builder.query({
